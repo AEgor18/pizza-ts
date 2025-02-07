9ec6ffb830a5624094f60b5c4867095e7570a510
= <aegor246@mail.ru>
2025-02-07 23:29:54 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
+import AddPizzaForm from './components/AddPizzaForm';
+import {Pizza} from "./models/Pizza";
+import DisplayPizzas from './components/DisplayPizzas';
 
 
-function App() {
+const App: FC = () => {
+  const [pizzasList, setPizzasList] = useState<Pizza[]>([]);
+
+  const addPizza = (newPizza: Pizza) => {
+    setPizzasList([...pizzasList, newPizza])
+  }
+  
+  const updatePizza = (newPizza: Pizza) => {
+    setPizzasList(pizzasList.map((pizza) => pizza.id === newPizza.id ? newPizza : pizza))
+  }
+
+  const deletePizza = (id: number) => {
+    setPizzasList(pizzasList.filter((pizza) => pizza.id !== id))
+  }
+
   return (
     <div className="App">
-      New App!
+      <div className="wrap">
+        <span className="heading">Наша пиццерия</span>
+        <AddPizzaForm addPizza={addPizza}/>
+        <DisplayPizzas pizzasList={pizzasList} updatePizza={updatePizza} deletePizza={deletePizza}/>
+      </div>
     </div>
   );
 }
